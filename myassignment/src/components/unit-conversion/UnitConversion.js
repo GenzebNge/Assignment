ec1e4ada52677b2fd81f7eeb7ddb0e48ec3a4ee0
GenzebNge <>
2020-05-13 23:02:35 -0400

    initial project set up

---
+++ myassignment/src/components/unit-conversion/UnitConversion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UnitConversion extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UnitConversion
---
+++ myassignment/src/components/unit-conversion/temperature/Temperatur.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Temperatur = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Temperatur
