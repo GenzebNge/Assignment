1d954d35b541083f4148f6d16214d0b7d53dc354
GenzebNge <>
2020-05-14 18:20:35 -0400

    volume unit populated to selection dropdown, temperature conversion computed, some button and input css added, volume input vlaue controled

--- myassignment/src/components/unit-conversion/temperature/Temperatur.js
+++ myassignment/src/components/unit-conversion/temperature/Temperatur.js
@@ -1,27 +1,44 @@
-import React from 'react'
+import React from "react";
+import './Temperature.css'
 
 const Temperature = (props) => {
-    const scaleOptionList = props.scales
-    .map((option) => {
-    return <option key ={option}>{option}</option>
-    })
-    return (
-        <div>
-            <h3>Temperature Conversion</h3>
-            <input type ="number" placeholder ="Enter The Value " onChange={props.inputHandle} />
-            {/* //pupulating the unit to the input filed */}
-            <select onChange={(e)=>props.changedFrom(e.target.value)}>
-                <option>...From...</option>
-                {scaleOptionList}
+  const scaleOptionList = props.scales.map((option) => {
+    return <option key={option}>{option}</option>;
+  });
+  return (
+    <div>
+      <h3>Temperature Unit Conversion</h3>
+      <form>
+        <label>Initial Temperature: </label>
+        <input className="input" 
+          type="number"
+          placeholder="Enter The Value "
+          onChange={props.inputHandle}
+        />
 
-            </select>
-            <select onChange={(e) =>props.ChangedTo(e.target.value)}>
-                <option>...To...</option>
-                {scaleOptionList}
-            </select>
+        {/* //pupulating the unit to the input filed */}
+        <select className ="select"onChange={props.changedFrom}>
+          <option>...From...</option>
+          {scaleOptionList}
+        </select>
+        <select className="select" onChange={props.ChangedTo}>
+          <option>...To...</option>
+          {scaleOptionList}
+        </select>
+      </form>
+      <button className="Button" onClick={props.converted}>Convert </button>
+      <div className="row">
+      <input type="number" placeholder="Enter Student Answer" onChange={props.answerHandled}/>
+      <select className="select" onChange={props.ChangedTo}>
+          <option>...In...</option>
+          {scaleOptionList}
+        </select>
+      <br/>
+      <button className="Btnc" onClick={props.compared}>Clicke To Check </button>
+      </div>
+     
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Temperature
+export default Temperature;
--- myassignment/src/components/unit-conversion/volume/Volume.js
+++ myassignment/src/components/unit-conversion/volume/Volume.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from "react";
+import './Volume.css'
 
- const  Volume = (props) => {
-    return (
-        <div>
-            <h3>Volume unit Conversion</h3>
-             <input type="number" placeholder="Enter The Value here "/>
-           
-        </div>
-    )
-}
-export default Volume
+const Volume = (props) => {
+    const volScalOptionList = props.volScales
+    .map(option =>{
+    return <option key={option}>{option}</option>
+    })
+  return (
+    <div>
+      <h3>Volume unit Conversion</h3>
+      <form>
+        <label>Initial Volume: </label>
+        <input className="input" type="number" placeholder="Enter The initial Volume" onChange={props.inputChaged}/>
+        <select>
+            <option>...From...</option>
+            {volScalOptionList}
+        </select>
+
+        <select>
+            <option>...TO...</option>
+            {volScalOptionList}
+        </select>
+      </form>
+      <button onClick={props.converted}>Clicke to convert</button>
+    </div>
+  );
+};
+export default Volume;
