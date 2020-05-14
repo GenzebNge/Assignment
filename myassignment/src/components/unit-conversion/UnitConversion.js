04b86567f7edda1c502fded88c267120b77418ae
GenzebNge <>
2020-05-14 00:12:51 -0400

    Temperature component input and unit selection handler impelimented

--- myassignment/src/components/unit-conversion/UnitConversion.js
+++ myassignment/src/components/unit-conversion/UnitConversion.js
@@ -8,14 +8,35 @@ export class UnitConversion extends Component {
     
         this.state = {
             tempScaleOptions : ["kelvine", "Fahrenheit", "Celsius", "Rankine"],
+            inputValue: 0,
+            convertedFrom: "",
+            convertedTo: "",
         }
     }
+
+    inputChangeHandler =(event) => {
+        this.setState({inputValue: event.target.value})
+        console.log(this.state.inputValue)
+    }
     
+    changedFromHandler = (selectedFrom) => {
+        this.setState({convertedFrom: selectedFrom})
+        console.log(this.state.convertedFrom)
+    }
+
+    changedToHandler =(selectedTo) => {
+        this.setState({convertedTo: selectedTo})
+        console.log(this.state.convertedTo)
+    }
+
     render() {
         return (
             <div className="container">
                 <div  className= "row">
-                <Temperature scales ={this.state.tempScaleOptions}/>
+                <Temperature scales ={this.state.tempScaleOptions}
+                inputHandle = {this.inputChangeHandler}
+                changedFrom={this.changedFromHandler}
+                ChangedTo={this.changedToHandler}/>
                 </div>
                 <div  className= "row">
                 <Volume/>
--- myassignment/src/components/unit-conversion/temperature/Temperatur.js
+++ myassignment/src/components/unit-conversion/temperature/Temperatur.js
@@ -8,14 +8,14 @@ const Temperature = (props) => {
     return (
         <div>
             <h3>Temperature Conversion</h3>
-            <input type ="number" placeholder ="Enter The Value " />
+            <input type ="number" placeholder ="Enter The Value " onChange={props.inputHandle} />
             {/* //pupulating the unit to the input filed */}
-            <select>
+            <select onChange={(e)=>props.changedFrom(e.target.value)}>
                 <option>...From...</option>
                 {scaleOptionList}
 
             </select>
-            <select>
+            <select onChange={(e) =>props.ChangedTo(e.target.value)}>
                 <option>...To...</option>
                 {scaleOptionList}
             </select>
